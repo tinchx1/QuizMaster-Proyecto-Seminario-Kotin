ec49ae40bf8379b8e3134d2b4b23e76df5355efc
Bruno Tendler <13393250+thermosflasche@users.noreply.github.com>
2024-04-19 19:45:13 -0300

    Agregar onClick a botones

--- app/src/main/java/com/example/quizmaster/MainActivity.kt
+++ app/src/main/java/com/example/quizmaster/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.quizmaster
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ScrollView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<ScrollView>(R.id.linear_main).children.filter { it is Button }.forEach {
+            view -> view.setOnClickListener { chooseCategory(view) }
+        }
+    }
+
+    private fun chooseCategory(v: View) {
     }
 }
