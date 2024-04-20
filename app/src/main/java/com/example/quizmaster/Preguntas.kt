67deb35974f9b10f3a3472d249c7441e62a7ea1b
Bruno Tendler <13393250+thermosflasche@users.noreply.github.com>
2024-04-20 15:16:00 -0300

    Inconsistencias menores en activity de preguntas

--- app/src/main/java/com/example/quizmaster/Preguntas.kt
+++ app/src/main/java/com/example/quizmaster/Preguntas.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class preguntas : AppCompatActivity() {
+class Preguntas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_preguntas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_preguntas)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
