8cd4e8ac4a49f81f41127ea6957c7b85a329044f
Bruno Tendler <13393250+thermosflasche@users.noreply.github.com>
2024-04-22 17:29:46 -0300

    Guardar y presentar preguntas

--- app/src/main/java/com/example/quizmaster/Preguntas.kt
+++ app/src/main/java/com/example/quizmaster/Preguntas.kt
@@ -1,7 +1,9 @@
 package com.example.quizmaster
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,12 @@ import org.json.JSONObject
 import java.io.InputStream
 
 class Preguntas : AppCompatActivity() {
+
+    private lateinit var buttons: Array<Button>
+    private lateinit var questions: List<Question>
+    private var questionIndex = 0
+    private var correctAnswers = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,13 +28,27 @@ class Preguntas : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val category = intent.getStringExtra("category") ?: ""
-        val textCategory = findViewById<TextView>(R.id.texto_categoria)
-        textCategory.text = category
-        loadQuestionsFromJSON(category)
+        findViewById<TextView>(R.id.texto_categoria).text = category
+
+        buttons = arrayOf(findViewById(R.id.opcion1), findViewById(R.id.opcion2), findViewById(R.id.opcion3), findViewById(R.id.opcion4))
+
+        buttons.forEach { it ->
+            it.setOnClickListener { answer(it as Button) }
+        }
+
+        questions = questionsFromJSON(category)
+        findViewById<TextView>(R.id.num_preguntas).text = questions.size.toString()
     }
 
-    private fun loadQuestionsFromJSON(category: String) {
+    override fun onStart() {
+        super.onStart()
+        askQuestion(questions[questionIndex])
+    }
+
+
+    private fun questionsFromJSON(category: String): List<Question> {
         val inputStream: InputStream = assets.open("questions.json")
         val size = inputStream.available()
         val buffer = ByteArray(size)
@@ -34,7 +56,12 @@ class Preguntas : AppCompatActivity() {
         inputStream.close()
         val text = String(buffer, Charsets.UTF_8)
         val jsonObject = JSONObject(text)
-        val questions = jsonObject.getJSONArray(category)
-        Log.d("Questions", questions.toString())
+        val questionsJson = jsonObject.getJSONArray(category)
+        Log.d("Questions", questionsJson.toString())
+
+        return List(questionsJson.length()) { i ->
+            Question(questionsJson.getJSONObject(i))
+        }
+    }
     }
 }
