c2b8b67da3e2699fa76d5ee746599c4d3ef20153
Bruno Tendler <13393250+thermosflasche@users.noreply.github.com>
2024-06-06 11:56:25 -0300

    Migrar User de Java a Kotlin

--- app/src/main/java/ar/unlp/quizmaster/User.kt
+++ app/src/main/java/ar/unlp/quizmaster/User.kt
@@ -1,35 +1,6 @@
-package ar.unlp.quizmaster;
+package ar.unlp.quizmaster
 
-public class User {
-    private String userName;
-    private int corrected = 0;
-    private int answered = 0;
-
-    public User(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public int getCorrected() {
-        return corrected;
-    }
-
-    public void setCorrected(int corrected) {
-        this.corrected = corrected;
-    }
-
-    public int getAnswered() {
-        return answered;
-    }
-
-    public void setAnswered(int answered) {
-        this.answered = answered;
-    }
+class User(var userName: String) {
+    var correct: Int = 0
+    var answered: Int = 0
 }
